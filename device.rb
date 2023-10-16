630f102109f2881d31c1af20c20a4d77a4d2a6d2
Yusaku <hitoyasablue@gmail.com>
2023-10-16 23:59:12 +0900

    create devise.rb

---
+++ device.rb
@@ -0,0 +1,16 @@
+class Device
+  attr_accessor :ip_address, :vlan
+
+  def initialize(ip_address, vlan = nil)
+    @ip_address = ip_address
+    @vlan = vlan
+  end
+
+  def ping(target)
+    if target.is_a?(Device)
+      return "#{target.ip_address} is reachable from #{@ip_address}"
+    else
+      return "Invalid target"
+    end
+  end
+end
